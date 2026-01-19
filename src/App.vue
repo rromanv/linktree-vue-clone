e1636b30504b21fe7e1b051c364c47543cd6fab0
Rodolfo Roman <rromanv@gmail.com>
2026-01-19 12:41:01 -0600

    Cahnged App.vue layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex min-h-screen flex-col">
+  <div class="min-h-screen bg-linear-to-br from-gray-900 via-gray-800 to-gray-900 font-sans">
     <RouterView />
   </div>
 </template>
